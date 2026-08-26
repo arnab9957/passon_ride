4253a3eccea842e262028780ec1ef57157b06207
Arnab Kumar Dey <arnabkumardey9957@gmail.com>
2026-08-26 20:13:00 +0530

    feat: implement widget preview module with custom theme, utilities, and asset support

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:60535/DHnMrrGsIt0=';
+const String kWidgetPreviewService =
+    'widget-preview-1f438abd-7b56-4e47-ac2f-90019f6bd396';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-1f438abd-7b56-4e47-ac2f-90019f6bd396';
+const String kProjectRootPath = r'D:\Desktop\Flutter\passon_ride';
